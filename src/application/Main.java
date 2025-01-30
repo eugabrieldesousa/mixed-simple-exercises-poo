5e099552ce27c56875468968787037430b044ac8
“eugabrieldesousa” <eugabrieldesousa@gmail.com>
2025-01-30 08:03:22 -0300

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,51 @@
+package application;
+
+import entities.Employee;
+import entities.Rectangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Exercicio do retangulo
+
+//        Rectangle retangulo1 = new Rectangle();
+//
+//        System.out.println("Enter rectangle width and height: ");
+//        System.out.println("width: ");
+//        retangulo1.width = sc.nextDouble();
+//        System.out.println("height: ");
+//        retangulo1.height = sc.nextDouble();
+//
+//        System.out.println("AREA = " + retangulo1.area());
+//        System.out.println("PERIMETER = " + retangulo1.perimeter());
+//        System.out.println("DIAGONAL = " + retangulo1.diagonal());
+
+        // Exercicio do empregado
+
+//        Employee empregado1 = new Employee();
+//
+//        System.out.println("Name: ");
+//        empregado1.name = sc.nextLine();
+//
+//        System.out.println("Gloss Salary: ");
+//        empregado1.grossSalary = sc.nextDouble();
+//        sc.nextLine();
+//
+//        System.out.println("Tax: ");
+//        empregado1.tax = sc.nextDouble();
+//        sc.nextLine();
+//
+//        System.out.printf("Employee: %s, $ %.2f \n", empregado1.name, empregado1.netSalary());
+//
+//        System.out.println("Which percentage to increase salary: ");
+//        empregado1.increaseSalary(sc.nextDouble());
+//
+//        System.out.println("Updated data: " + empregado1);
+
+        sc.close();
+    }
+}
